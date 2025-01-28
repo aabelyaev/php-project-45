e21fc3a1de4a08d3c741912ce339cd3459d1f70a
aabelyaev <a.belyaev87@bk.ru>
2025-01-28 20:47:54 +0500

    Change

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,3 @@
 <?php
 
 namespace Project\Cli;
-
-class Cli {
-    public function greet()
-    {
-        echo "Welcome to the Brain Games!\n";
-        $name = trim(fgets(STDIN));
-        echo "Hello, $name!\n";
-    }
-}
