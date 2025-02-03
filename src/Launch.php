c8ece5071dd14c500246e7e7308dac63cf277d2a
aabelyaev <a.belyaev87@bk.ru>
2025-02-03 19:42:45 +0500

    gdsf

--- src/Launch.php
+++ src/Launch.php
@@ -14,16 +14,31 @@ function run(string $gameDescription, callable $round)
     line($gameDescription);
 
     $circle = 3;
-    for ($i = 0; $i < $circle; $i++) {
+    for ($i = 0; ; ) {
         [$answer, $correctAnswer] = $round();
         if ($answer == $correctAnswer) {
             line('Correct!');
+            goto start;
         } else {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
             line("Let's try again, {$name}!");
-            run($gameDescription, $round);
+            break;
         }
     }
+
+start:
+    for ($i = 0; $i < $circle - 1; $i++) {
+        [$answer, $correctAnswer] = $round();
+        if ($answer == $correctAnswer) {
+            line('Correct!');
+            goto start;
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, {$name}!");
+        }
+    }
+
+    line("Congratulations, {$name}!");
 }
 //Весь вывод и логика в случае правильного и неправильного ответа повторяют предыдущие шаги.Либо через цикл while 
 // function run(string $gameDescription, $round) {
