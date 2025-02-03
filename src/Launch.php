3329ef4766f40616dc34a11a460696bd563db73d
aabelyaev <a.belyaev87@bk.ru>
2025-02-03 19:21:06 +0500

    sdfas

--- src/Launch.php
+++ src/Launch.php
@@ -14,15 +14,32 @@ function run(string $gameDescription, callable $round)
     line($gameDescription);
 
     $circle = 3;
-    for ($i = 0; $i < $circle; $i++) {
-        [$answer, $correctAnswer] = $round();
-        if ($answer == $correctAnswer) {
-            line('Correct!');
-        } else {
-            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
-            line("Let's try again, {$name}!");
-            break;
-        }
+for ($i = 0; $i < $circle; $i++) {
+    [$answer, $correctAnswer] = $round();
+    if ($answer == $correctAnswer) {
+        line('Correct!');
+    } else {
+        line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+        line("Let's try again, {$name}!");
+        break;
     }
-    return run($gameDescription, $round);
 }
+        line("Congratulations, {$name}!");
+}
+//Весь вывод и логика в случае правильного и неправильного ответа повторяют предыдущие шаги.Либо через цикл while 
+// function run(string $gameDescription, $round) {
+//     line("Welcome to the Brain Games!");
+//     $name = prompt("May I have your name?");
+//     line("Hello {$name}");
+//     line($gameDescription);
+
+//     while (true) { 
+//         [$answer, $correctAnswer] = $round();
+//         if ($answer == $correctAnswer) {
+//             line('Correct!');
+//         } else {
+//             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+//             line("Let's try again, {$name}!");
+//         }
+//     }
+// }
