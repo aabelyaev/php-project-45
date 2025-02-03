6768927ba585b3afb7923c5a76bb26278e1996d3
aabelyaev <a.belyaev87@bk.ru>
2025-02-03 19:14:55 +0500

    dasda

--- src/Launch.php
+++ src/Launch.php
@@ -24,21 +24,5 @@ function run(string $gameDescription, callable $round)
             break;
         }
     }
+    return run($gameDescription, $round);
 }
-//Весь вывод и логика в случае правильного и неправильного ответа повторяют предыдущие шаги.Либо через цикл while 
-// function run(string $gameDescription, $round) {
-//     line("Welcome to the Brain Games!");
-//     $name = prompt("May I have your name?");
-//     line("Hello {$name}");
-//     line($gameDescription);
-
-//     while (true) { 
-//         [$answer, $correctAnswer] = $round();
-//         if ($answer == $correctAnswer) {
-//             line('Correct!');
-//         } else {
-//             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
-//             line("Let's try again, {$name}!");
-//         }
-//     }
-// }
