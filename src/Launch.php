2d2bc24275a20188f28e2b07c83f40bb0542924c
aabelyaev <a.belyaev87@bk.ru>
2025-02-03 17:30:58 +0500

    change

--- src/Launch.php
+++ src/Launch.php
@@ -6,40 +6,32 @@ use function cli\line;
 use function cli\prompt;
 
 
-const GAME_ROUND_COUNT = 3;
-
-/**
- * Main cycle of game. User have to introduce himself and answer questions.
- * @param string $questionString - question text
- * @param array $gameResults - array with game data
- */
-function runGame(string $questionString, array $gameResults): void
+function run(string $gameDescription, $round)
 {
-    line("Welcome to the Brain Games!\n");
+    line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
+    line($gameDescription);
 
-    foreach ($gameResults as $gameResult) {
-        $question = $gameResult["question"];
-        line("{$questionString} \nQuestion: {$question}");
-        $correctAnswer = $gameResult["answer"];
-        $userAnswer = prompt("Your answer");
+    $countRounds = 3;
+    $colCorrect = 0;
+    for ($i = 0; $i < $countRounds; $i++) {
+        [$question, $correctAnswer] = $conditionsGame();
+        line("Question: %s", $question);
+        $answer = prompt('Your answer ');
 
-        if (!checkAnswer($userAnswer, $correctAnswer)) {
-            line("'{$userAnswer}' is wrong answer ;(. 
-            Correct answer was '{$correctAnswer}'.");
-            line("Let's try again, {$name}!");
-            return;
+        if ($answer == $correctAnswer) {
+            line('Correct!');
+            $colCorrect++;
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+            line("Let's try again, %s!", $name);
+            break;
         }
-        line("Correct!");
     }
-    line("Congratulations, {$name}!");
-}
-
-
-function checkAnswer(string $userAnswer, string|int $correctAnswer): bool
-{
-    return $userAnswer === (string)$correctAnswer;
+    if ($colCorrect === 3) {
+        line("Congratulations, %s!", $name);
+    }
 }
 //Весь вывод и логика в случае правильного и неправильного ответа повторяют предыдущие шаги.Либо через цикл while 
 // function run(string $gameDescription, $round) {
