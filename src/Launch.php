39030d719f2e8c4e671d8b443d7d9bea7dbf2702
aabelyaev <a.belyaev87@bk.ru>
2025-02-03 17:10:13 +0500

    change

--- src/Launch.php
+++ src/Launch.php
@@ -6,40 +6,19 @@ use function cli\line;
 use function cli\prompt;
 
 
-function run(string $gameDescription, $round)
-{
+function run(string $gameDescription, $round) {
     line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
-    line("Hello, {$name}");
+    line("Hello {$name}");
     line($gameDescription);
 
-    $circle = 3;
-    for ($i = 0; $i < $circle; $i++) {
+    while (true) { 
         [$answer, $correctAnswer] = $round();
         if ($answer == $correctAnswer) {
             line('Correct!');
         } else {
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
             line("Let's try again, {$name}!");
-            break;
         }
     }
-    line("Congratulations, {$name}!");
 }
-//Весь вывод и логика в случае правильного и неправильного ответа повторяют предыдущие шаги.Либо через цикл while 
-// function run(string $gameDescription, $round) {
-//     line("Welcome to the Brain Games!");
-//     $name = prompt("May I have your name?");
-//     line("Hello {$name}");
-//     line($gameDescription);
-
-//     while (true) { 
-//         [$answer, $correctAnswer] = $round();
-//         if ($answer == $correctAnswer) {
-//             line('Correct!');
-//         } else {
-//             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
-//             line("Let's try again, {$name}!");
-//         }
-//     }
-// }
