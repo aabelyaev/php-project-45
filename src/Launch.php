c6edcb308a37967b6c3b95b6a364cc88cee6e110
aabelyaev <a.belyaev87@bk.ru>
2025-02-03 16:07:25 +0500

    change

--- src/Launch.php
+++ src/Launch.php
@@ -13,7 +13,7 @@ function run(string $gameDescription, $round)
     line("Hello {$name}");
     line($gameDescription);
 
-    $circle = 3;
+    $circle = [];
     for ($i = 0; $i < $circle; $i++) {
         [$answer, $correctAnswer] = $round();
         if ($answer == $correctAnswer) {
@@ -22,7 +22,6 @@ function run(string $gameDescription, $round)
             line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
             line("Let's try again, {$name}!");
         }
-        break;
     }
 
     line("Congratulations, {$name}!");
