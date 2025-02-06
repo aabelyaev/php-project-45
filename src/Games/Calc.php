b7cedc3a5d4791e48224ac0fe603039141ddc281
aabelyaev <a.belyaev87@bk.ru>
2025-02-06 19:13:55 +0500

    change

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -15,11 +15,12 @@ function play()
         $operationId = rand(0, count($operations) - 1);
         switch ($operations[$operationId]) {
             case '+':
-                return $x + $y;
+                $output = $x + $y;
             case '-':
-                return  $x - $y;
+                $output = $x - $y;
+                break;
             case '*':
-                return $x * $y;
+                $output = $x * $y;
             default:
                 break;
         }
@@ -27,7 +28,7 @@ function play()
         echo "Question: $expression\n";
         $answer = (int)readline();
 
-        return [$answer, $expression];
+        return [$answer, $output];
     };
 
     run($description, $getAnswer);
